4828ea55869e0522b329ee6bcdfb6dcc316fcb3e
DeemaJS <deeema@outlook.com>
2015-11-01 17:20:32 +0200

    Cheerio(DOM-parser) and request module

--- main_req.js
+++ main_req.js
@@ -1,6 +1,9 @@
 var request = require('request');
+var cheerio = require('cheerio');
 
 request({uri:'http://rod.dn.ua', method:'GET', encoding:'binary'},
 	function (err, res, page) {
-		console.log(page);
+		var $ = cheerio.load(page);
+		classMenu = $('<ul class="menu">');
+		console.log(classMenu);
 	});
