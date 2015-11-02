b63b5860b354bc45ffd8e8d28b34019248f39eb9
DeemaJS <deeema@outlook.com>
2015-11-02 16:22:21 +0200

    Cheerio more task

--- main_req.js
+++ main_req.js
@@ -4,6 +4,12 @@ var cheerio = require('cheerio');
 request({uri:'http://rod.dn.ua', method:'GET', encoding:'binary'},
 	function (err, res, page) {
 		var $ = cheerio.load(page);
-		classMenu = $('<ul class="menu">');
-		console.log(classMenu);
+		var links = [];
+
+		$('a').each(function(i, elem) {
+			links[i] = $(this).attr('href');
+		});
+
+		links.join(', ');
+		console.log(links);
 	});
